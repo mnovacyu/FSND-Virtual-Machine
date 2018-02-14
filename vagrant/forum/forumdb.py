4dde68d7ecdec0f00769c67ed58215975e583e53
Michael Novac-Yu <michael.novacyu@teamtreehouse.com>
2018-02-13 17:36:13 -0800

    add bleach security support

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,19 +1,25 @@
 # "Database code" for the DB Forum.
 
+import bleach
 import datetime
 import psycopg2
 
-conn = psycopg2.connect("dbname=forum")
-
-cursor = conn.cursor()
+DBNAME = "forum"
 
 def get_posts():
   """Return all posts from the 'database', most recent first."""
+  conn = psycopg2.connect(database=DBNAME)
+  cursor = conn.cursor()
   cursor.execute("SELECT content, time FROM posts ORDER BY time desc")
   posts = cursor.fetchall()
   return posts
+  conn.close()
 
 def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
-  cursor.execute("INSERT INTO posts (content, time) VALUES (%s, %s)", (content, datetime.datetime.now()))
+  conn = psycopg2.connect(database=DBNAME)
+  cursor = conn.cursor()
+  cleaned_text = bleach.clean(content)
+  cursor.execute("INSERT INTO posts (content, time) VALUES (%s, %s)", (cleaned_text, datetime.datetime.now()))
   conn.commit()
+  conn.close()
